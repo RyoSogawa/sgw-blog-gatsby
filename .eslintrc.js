dd024a01c2d14c139386cd587e066c78415845e3
RyoSogawa <koashimitekara.1122@gmail.com>
2021-12-27 22:36:03 +0900

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:tailwindcss/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint', 'react', 'tailwindcss'],
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    tailwindcss: {
+      // These are the default values but feel free to customize
+      callees: ['clsx'],
+      config: 'tailwind.config.js',
+      prependCustom: false,
+      removeDuplicates: true,
+      whitelist: [],
+    },
+  },
+  rules: {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-no-target-blank': 2,
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
+    ],
+    'object-shorthand': ['error', 'properties'],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'tailwindcss/classnames-order': 'warn',
+    'tailwindcss/no-custom-classname': 'off',
+    'tailwindcss/no-contradicting-classname': 'error',
+  },
+}
