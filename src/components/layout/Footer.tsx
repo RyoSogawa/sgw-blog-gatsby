4bc284359b1c0c4be7d6429fd4cf700aac302956
RyoSogawa <koashimitekara.1122@gmail.com>
2022-01-15 23:41:00 +0900

    [fix] Footerの崩れ修正

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,22 +4,21 @@ import Link from '../uiParts/Link'
 
 const Footer = (): JSX.Element => {
   return (
-    <>
+    <footer className={'sticky top-full'}>
       <div className="mt-20 text-center">
         <Link to="https://www.buymeacoffee.com/RyoSogawa">
           <StaticImage
             src="https://cdn.buymeacoffee.com/buttons/v2/default-blue.png"
             alt="Buy Me A Coffee"
             className={
-              'rounded-xl shadow-md hover:shadow-lg transition-shadow hover:-translate-y-0.5 transition-translate h-[60px] w-[217px]'
+              'rounded-2xl shadow-md hover:shadow-lg transition-shadow hover:-translate-y-0.5 transition-translate h-[60px] w-[217px]'
             }
-            placeholder={'dominantColor'}
           />
         </Link>
       </div>
-      <footer
+      <div
         className={
-          'sticky top-full py-4 mt-10 text-xs text-gray-600 dark:text-gray-400 bg-gray-100 border-t dark:bg-slate-800 dark:border-slate-600'
+          'py-4 mt-10 text-xs text-gray-600 dark:text-gray-400 bg-gray-100 border-t dark:bg-slate-800 dark:border-slate-600'
         }
       >
         <div className={'container flex gap-2 justify-between items-center'}>
@@ -72,8 +71,8 @@ const Footer = (): JSX.Element => {
           <div className={'grow'} />
           <span>&copy; RyoSogawa</span>
         </div>
-      </footer>
-    </>
+      </div>
+    </footer>
   )
 }
 
